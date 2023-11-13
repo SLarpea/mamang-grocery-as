1f3aa7cc21e4dc5af543076d2ab52e7c686e8f97
Arnold <sulanaarnold@gmail.com>
2023-11-13 20:09:37 +0800

    added design on carousel page

--- resources/js/Pages/Admin/Carousels.vue
+++ resources/js/Pages/Admin/Carousels.vue
@@ -4,9 +4,195 @@ import Modal from "@/Components/Admin/Modal.vue";
 import { onMounted, reactive, ref } from "vue";
 import { useForm } from "@inertiajs/vue3";
 
+const expanded = ref([]);
+const headers = ref([
+  {
+    title: "Dessert (100g serving)",
+    align: "start",
+    sortable: false,
+    key: "name",
+  },
+  { title: "Calories", key: "calories" },
+  { title: "Fat (g)", key: "fat" },
+  { title: "Carbs (g)", key: "carbs" },
+  { title: "Protein (g)", key: "protein" },
+  { title: "Iron (%)", key: "iron" },
+  { title: "", key: "data-table-expand" },
+]);
+const items = ref([
+  {
+    name: "Frozen Yogurt",
+    calories: 159,
+    fat: 6.0,
+    carbs: 24,
+    protein: 4.0,
+    iron: 1,
+  },
+  {
+    name: "Ice cream sandwich",
+    calories: 237,
+    fat: 9.0,
+    carbs: 37,
+    protein: 4.3,
+    iron: 1,
+  },
+  {
+    name: "Eclair",
+    calories: 262,
+    fat: 16.0,
+    carbs: 23,
+    protein: 6.0,
+    iron: 7,
+  },
+  {
+    name: "Cupcake",
+    calories: 305,
+    fat: 3.7,
+    carbs: 67,
+    protein: 4.3,
+    iron: 8,
+  },
+  {
+    name: "Gingerbread",
+    calories: 356,
+    fat: 16.0,
+    carbs: 49,
+    protein: 3.9,
+    iron: 16,
+  },
+  {
+    name: "Jelly bean",
+    calories: 375,
+    fat: 0.0,
+    carbs: 94,
+    protein: 0.0,
+    iron: 0,
+  },
+  {
+    name: "Lollipop",
+    calories: 392,
+    fat: 0.2,
+    carbs: 98,
+    protein: 0,
+    iron: 2,
+  },
+  {
+    name: "Honeycomb",
+    calories: 408,
+    fat: 3.2,
+    carbs: 87,
+    protein: 6.5,
+    iron: 45,
+  },
+  {
+    name: "Donut",
+    calories: 452,
+    fat: 25.0,
+    carbs: 51,
+    protein: 4.9,
+    iron: 22,
+  },
+  {
+    name: "KitKat",
+    calories: 518,
+    fat: 26.0,
+    carbs: 65,
+    protein: 7,
+    iron: 6,
+  },
+]);
+
+const crumbs = ref([
+    {
+          title: 'Dashboard',
+          disabled: false,
+          href: 'breadcrumbs_dashboard',
+        },
+        {
+          title: 'Link 1',
+          disabled: false,
+          href: 'breadcrumbs_link_1',
+        },
+        {
+          title: 'Link 2',
+          disabled: true,
+          href: 'breadcrumbs_link_2',
+        },
+]);
 </script>
 <template>
-    <AdminLayout title="Carousels">
-        <h1>Carousels</h1>
-    </AdminLayout>
+  <AdminLayout title="Carousels">
+    <div class="wrapper">
+        <v-breadcrumbs :items="crumbs">
+    <template v-slot:prepend>
+      <v-icon size="small" icon="$vuetify"></v-icon>
+    </template>
+  </v-breadcrumbs>
+      <v-sheet
+        class="categories p-5"
+        height="auto"
+        width="100%"
+        style="
+          box-shadow: rgba(0, 0, 0, 0.15) 0 0.3rem 0.2rem 0;
+          border-bottom: 0.3rem solid #6a1b9a;
+        "
+        border
+        rounded
+      >
+        <v-row
+          style="
+            height: 5rem;
+            display: flex;
+            align-items: center;
+            margin-bottom: 1.3rem;
+          "
+          no-gutters
+        >
+          <v-col cols="11">
+            <span class="card-title">Carousels</span>
+            <span class="card-subtitle"
+              >Create your own carousel design for homepage.</span
+            >
+          </v-col>
+          <v-col cols="1" class="text-right">
+            <v-btn
+              size="small"
+              color="add-bg-button-color"
+              prepend-icon="fa-solid fa-circle-plus"
+              @click="openModal('add')"
+            >
+              add carousel
+            </v-btn>
+          </v-col>
+        </v-row>
+        <v-row no-gutters>
+          <v-col cols="12">
+            <v-data-table
+              v-model:expanded="expanded"
+              :headers="headers"
+              :items="items"
+              item-value="name"
+              show-expand
+            >
+              <template v-slot:top>
+                <v-toolbar flat>
+                  <v-toolbar-title>Expandable Table</v-toolbar-title>
+                </v-toolbar>
+              </template>
+              <template v-slot:expanded-row="{ columns, item }">
+                <tr>
+                  <td>
+                    More info about {{ item.name }}
+                  </td>
+                  <td>
+                    More info about {{ item.name }}
+                  </td>
+                </tr>
+              </template>
+            </v-data-table>
+          </v-col>
+        </v-row>
+      </v-sheet>
+    </div>
+  </AdminLayout>
 </template>
